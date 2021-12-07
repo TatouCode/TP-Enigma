cfe5ba2c6a747417f782d82a3e6a28e2da4595b7
Tebenj <65290489+Tebenj@users.noreply.github.com>
2021-12-07 10:30:18 +0100

    Update avec les std

--- encrypt.h
+++ encrypt.h
@@ -5,21 +5,21 @@ namespace Crypt{
 
 class Encrypt {
 public:
-	Encrypt(string const _nomFichierRead, string const _nomFichierWrite, string _plain, string _cipher);
-	std::string plain();
-	std::string cipher();
-	std::string const read(string const _nomFichier);
-	std::string const write(string const _nomFichier);
-	virtual std::string const decode(string const _nomFichier) = 0;
-	virtual std::string const encode(string const _nomFichier) = 0;
+	Encrypt(std::string const _nomFichierRead, std::string const _nomFichierWrite, std::string _plain, std::string _cipher);
+	std::string getPlain();
+	std::string getCipher();
+	std::string const read(std::string const _nomFichier);
+	std::string const write(std::string const _nomFichier);
+	virtual std::string const decode(std::string const _nomFichier) = 0;
+	virtual std::string const encode(std::string const _nomFichier) = 0;
 
 private:
-	string const _nomFichierRead;
-	string const _nomFichierWrite;
+	std::string const _nomFichierRead;
+	std::string const _nomFichierWrite;
 
 protected:
-	string _plain;
-	string _cipher;
+	std::string _plain;
+	std::string _cipher;
 
 
 }
